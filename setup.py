27e5bcfac091bf4919523dbae97ef6746402d1f8
GunWoo Choi <6566gun@gmail.com>
2014-03-17 01:27:16 +0900

    Version update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
 ]
 
 setup(name='Flask-Negotiation',
-      version='0.1.5',
+      version='0.1.6',
       url='http://blog.hardtack.me/',
       author='GunWoo Choi',
       author_email='6566gun@gmail.com',
